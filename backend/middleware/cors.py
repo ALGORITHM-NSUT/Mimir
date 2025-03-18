b1d2fcd9bd5e0cdd6e6cde179ea7bdf5a53556d9
Saumil004 <agg.sa31082004@gmail.com>
2025-03-18 09:45:44 +0530

    fixed cors policy

--- backend/middleware/cors.py
+++ backend/middleware/cors.py
@@ -1,9 +1,12 @@
 from fastapi.middleware.cors import CORSMiddleware
 
+allowedorigins = ["https://mimir-frontend-eight.vercel.app", "http://localhost:5173"]
+
+
 def setup_cors(app):
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["*"],  
+        allow_origins= allowedorigins,  
         allow_credentials=True,
         allow_methods=["*"],  
         allow_headers=["*"],  
